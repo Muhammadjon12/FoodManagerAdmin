4882b756fa27918fbade0a1fd076c8bd51e5a4bc
Muhammadjon12 <Muhammadumirzoqov12@gmail.com>
2021-04-09 10:20:06 +0500

    used for connection error "try" and "using"

--- FoodManagerAdmin/Database/Database.cs
+++ FoodManagerAdmin/Database/Database.cs
@@ -10,7 +10,7 @@ using FoodManagerAdmin.Model;
 
 namespace FoodManagerAdmin.Database
 {
-     class Database
+    class Database
     {
         public MySqlConnection conn;
         MySqlCommand cmd = new MySqlCommand();
@@ -33,22 +33,20 @@ namespace FoodManagerAdmin.Database
                 conn = new MySqlConnection(connString);
 
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                MessageBox.Show("Дар пайвастшави хатоги мавҷуд");
+                MessageBox.Show("Дар пайвастшави хатоги мавҷуд"+ex.Message);
             }
-
         }
-  
         public List<Food> GetAllFood()
         {
             ListFood.Clear();
-                conn.Open();
-                cmd.CommandText = "select id,name,price,typesFood,date,descr,image from food_table";
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = conn;
-                reader = cmd.ExecuteReader();
-                
+            try
+            {
+                using (MySqlCommand cmd = new MySqlCommand("select id,name,price,typesFood,date,descr,image from food_table", conn))
+                {
+                    conn.Open();
+                    reader = cmd.ExecuteReader();
                     while (reader.Read())
                     {
                         Food food = new Food();
@@ -60,82 +58,135 @@ namespace FoodManagerAdmin.Database
                         food.Descr = reader.GetString(5);
 
                         food.Image = (byte[])reader["image"];
-                      
+
                         ListFood.Add(food);
                     }
-                conn.Close();
-                return ListFood;
+                    conn.Close();
+                    return ListFood;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
+            return null;
 
         }
-   
+
         public void SaveFood(Food food)
         {
-                conn.Open();
-                cmd.CommandText = "INSERT INTO food_table(name, price,typesFood,image,date,descr) VALUES(@name, @price,@typesFood,@image,@date,@descr)";
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = conn;
+            try
+            {   
+                using (MySqlCommand cmd = new MySqlCommand("INSERT INTO food_table(name, price,typesFood,image,date,descr) VALUES(@name, @price,@typesFood,@image,@date,@descr)", conn))
+                { 
+                    cmd.Parameters.Add("@name", MySqlDbType.VarChar).Value = food.Name;
+                    cmd.Parameters.Add("@price", MySqlDbType.Double).Value = food.Price;
+                    cmd.Parameters.Add("@descr", MySqlDbType.VarChar).Value = food.Descr;
+                    cmd.Parameters.Add("@typesFood", MySqlDbType.VarChar).Value = food.Typefood;
+                    cmd.Parameters.Add("@image", MySqlDbType.Blob).Value = food.Image;
+                    cmd.Parameters.Add("@date", MySqlDbType.DateTime).Value = food.DateTime;
 
-                cmd.Parameters.Add("@name", MySqlDbType.VarChar).Value = food.Name;
-                cmd.Parameters.Add("@price", MySqlDbType.Double).Value = food.Price;
-                cmd.Parameters.Add("@descr", MySqlDbType.VarChar).Value = food.Descr;
-                cmd.Parameters.Add("@typesFood", MySqlDbType.VarChar).Value = food.Typefood;
-                cmd.Parameters.Add("@image", MySqlDbType.Blob).Value = food.Image;
-                cmd.Parameters.Add("@date", MySqlDbType.DateTime).Value = food.DateTime;
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
 
-                cmd.ExecuteNonQuery();
-                conn.Close();
 
         }
-        public void UpdateFood(int id,string name, Double price,string typefood,string descr,byte[] image)
+        public void UpdateFood(int id, string name, Double price, string typefood, string descr, byte[] image)
         {
-                conn.Open();
-                cmd.CommandText = "UPDATE food_table SET name=@name,price=@price,typesFood=@typesFood,descr=@descr,image=@image WHERE id = @id";
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = conn;
+            try
+            {
+                using (MySqlCommand cmd = new MySqlCommand("UPDATE food_table SET name=@name,price=@price,typesFood=@typesFood,descr=@descr,image=@image WHERE id = @id", conn))
+                {
+                    cmd.Parameters.Add("@name", MySqlDbType.VarChar).Value = name;
+                    cmd.Parameters.Add("@price", MySqlDbType.Int16).Value = price;
+                    cmd.Parameters.Add("@descr", MySqlDbType.VarChar).Value = descr;
+                    cmd.Parameters.Add("@image", MySqlDbType.Blob).Value = image;
+                    cmd.Parameters.Add("@typesFood", MySqlDbType.VarChar).Value = typefood;
+                    cmd.Parameters.Add("@id", MySqlDbType.Int16).Value = id;
 
-                cmd.Parameters.Add("@name", MySqlDbType.VarChar).Value = name;
-                cmd.Parameters.Add("@price", MySqlDbType.Int16).Value = price;
-                cmd.Parameters.Add("@descr", MySqlDbType.VarChar).Value = descr;
-                cmd.Parameters.Add("@image", MySqlDbType.Blob).Value = image;
-                cmd.Parameters.Add("@typesFood", MySqlDbType.VarChar).Value = typefood;
-                cmd.Parameters.Add("@id", MySqlDbType.Int16).Value = id;
-
-                cmd.ExecuteNonQuery();
-                conn.Close();
-
-         }
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
+        }
         public void DeleteFoodByID(int id)
         {
-                conn.Open();
-                cmd.CommandText = "DELETE from food_table  WHERE id = @id";
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = conn;
+            try
+            {
+              using (MySqlCommand cmd = new MySqlCommand("DELETE from food_table  WHERE id = @id", conn))
+                {
+                    cmd.Parameters.Add("@id", MySqlDbType.Int16).Value = id;
 
-                cmd.Parameters.Add("@id", MySqlDbType.Int16).Value = id;
-
-                cmd.ExecuteNonQuery();
-                conn.Close();
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
         }
 
-            public List<string> GetFoodTypes()
+        public List<string> GetFoodTypes()
+        {
+            try
             {
                 list.Clear();
-                    conn.Open();
-                    cmd.CommandText = "select * from types_food";
-                    cmd.CommandType = CommandType.Text;
-                    cmd.Connection = conn;
 
+                using (MySqlCommand cmd = new MySqlCommand("select * from types_food", conn))
+                {
+                    conn.Open();
                     reader = cmd.ExecuteReader();
 
                     while (reader.Read())
                     {
                         list.Add(reader[1].ToString());
                     }
-                
+
                     conn.Close();
                     return list;
+                }
             }
-      }
+            catch (Exception ex)
+            { 
+                MessageBox.Show(ex.Message+"\n Ба база пайваст нест");
+            }
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
+            return null;
+        }
     }
+}
 
 
